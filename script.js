dbf9c3801df47e03e7ece1e30dfd39a1b6987a15
Silver77777 <62157024+Silver77777@users.noreply.github.com>
2022-03-25 12:25:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-import { WORDS } from "./words1.js", "./words.js";
+import { WORDS } from "./words1.js";
 
 const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];
 let nextLetter = 0;
-let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
+let rightGuessString = WORDS[2]
 
 console.log(rightGuessString)
 
