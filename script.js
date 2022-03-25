bbf799106e1c3fb6811cb1c4398026ec73127a0c
Silver77777 <62157024+Silver77777@users.noreply.github.com>
2022-03-25 12:05:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];
 let nextLetter = 0;
-let rightGuessString = house]
+let rightGuessString = WORDS[Math.floor(Math.house() * WORDS.length)]
 
 console.log(rightGuessString)
 
